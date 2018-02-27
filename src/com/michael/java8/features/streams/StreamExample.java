85507a8aab399effa38ea73593c72ed5d407b2a9
MICHAEL PHILOMI <MichaelPhilomi.PaneerSelvam@merrillcorp.com>
2018-02-27 12:26:09 +0530

    First Commit Of Java8 Feature Practice

---
+++ src/com/michael/java8/features/streams/StreamExample.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.michael.java8.features.streams;
+
+/**
+ * @author 109726
+ *
+ */
+public class StreamExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+        // Add code here.
+	}
+
+}

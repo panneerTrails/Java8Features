1696106e955eca086fbfdb9bfa105d810531c80b
MICHAEL PHILOMI <MichaelPhilomi.PaneerSelvam@merrillcorp.com>
2018-03-19 20:04:19 +0530

    Adding Java8 Stream Features Code Samples

--- src/com/michael/java8/features/streams/StreamFilterWithMultipleConditions.java
+++ src/com/michael/java8/features/streams/StreamFilterWithMultipleConditions.java
@@ -5,7 +5,6 @@ package com.michael.java8.features.streams;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class StreamFilterWithMultipleConditions {
 
